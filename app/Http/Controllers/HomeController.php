a8fef1b469513bed0695e685aea372c1c3ab9e3f
Karina Nisoria <karinanisoria@gmail.com>
2023-10-13 19:32:52 -0300

    Productos y categorias

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('welcome');
+        $productos = Producto::all();
+        return view('welcome')->with(compact('productos'));
     }
 }
